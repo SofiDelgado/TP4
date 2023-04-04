abc953cf2e8b4a91d100203097d2824a6af968e7
Sofi Delgado <sofi5_09@hotmail.com>
2023-04-03 23:50:43 -0300

    ejercicio numero 1

---
+++ js/Ejercicio-1.js
@@ -0,0 +1,9 @@
+let auto = {
+    color: 'rojo',
+    marca: 'fitito',
+    modelo: '1900',
+    encender:()=>document.write('<p>El auto esta encendido</p>'),
+    apagar: ()=>document.write('<p>El auto esta apagado</p>')
+}
+auto.encender();
+auto.apagar();
